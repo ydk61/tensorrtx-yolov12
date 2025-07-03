d091cc2c61b9d82d58c90ea4148f5d5efcb0f3d1
ydk61 <113221630+ydk61@users.noreply.github.com>
2025-07-03 20:24:39 +0800

    Update model.h

--- include/model.h
+++ include/model.h
@@ -11,3 +11,7 @@ nvinfer1::IHostMemory* buildEngineYolov12Det(nvinfer1::IBuilder* builder, nvinfe
 nvinfer1::IHostMemory* buildEngineYolov12Seg(nvinfer1::IBuilder* builder, nvinfer1::IBuilderConfig* config,
                                             nvinfer1::DataType dt, const std::string& wts_path, float& gd, float& gw,
                                             int& max_channels, std::string& type);
+
+nvinfer1::IHostMemory* buildEngineYolov12Cls(nvinfer1::IBuilder* builder, nvinfer1::IBuilderConfig* config,
+                                            nvinfer1::DataType dt, const std::string& wts_path, float& gd, float& gw,
+                                            std::string& type, int max_channels);
